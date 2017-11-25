b620915527acda9de2b5b349c3289becc37ab2f7
Jamik Karimov <jkarimov1593@gmail.com>
2017-11-25 17:24:24 +0300

    modified

--- quickSort.js
+++ quickSort.js
@@ -1,50 +1,47 @@
-		/*
-    Быстрая сортировка
-    Время выполнения:
-        средний случай O(nlogn)
-        худший случай O(n^2)
-    */
+/*
+Быстрая сортировка
+Время выполнения:
+    средний случай O(nlogn)
+    худший случай O(n^2)
+*/
     function quickSort(arr) {
-			let len = arr.length;
-			//Базовый случай, массив уже отсортирован
-			if (len < 2) {
-				return arr;
-			}
+	let len = arr.length;
+	//Базовый случай, массив уже отсортирован
+	if (len < 2) {
+		return arr;
+	}
 
-			let i = len - 2;
-			let less = [];
-			let greater = [];
-			let del = arr.splice(Math.round(len / 2), 1);
+	let i = len - 2;
+	let less = [];
+	let greater = [];
+	let del = arr.splice(Math.round(len / 2), 1);
       
       //Точка опоры => средний элемент
-			let pivot = +del.join();
-
-			// debugger;
-
-			while (i >= 0) {
+	let pivot = +del.join();
+		
+	while (i >= 0) {
 				
-				if (arr[i] <= pivot) {
-					//Подмассив всех элементов, мешьше опорного
-					less.push(arr[i]);
-				}
-
-				if (arr[i] > pivot) {
-					//Подмассив всех элементов, больше опорного
-					greater.push(arr[i]);
-				}
-
-				i--;
-			}
-
-			return [...quickSort(less), ...[pivot], ...quickSort(greater)];
+		if (arr[i] <= pivot) {
+			//Подмассив всех элементов, мешьше опорного
+			less.push(arr[i]);
 		}
 
+		if (arr[i] > pivot) {
+			//Подмассив всех элементов, больше опорного
+			greater.push(arr[i]);
+		}
 
-		var array = [2, 4, 7, 3, 12, 11, 23, 45, 23, 1, 5, 7, 7, 3, 34, 456, 678, 23434, 676, 32, 12, 1, 3, 4, 5];
+		i--;
+	}
+
+	return [...quickSort(less), ...[pivot], ...quickSort(greater)];
+}
 
 
-		console.time('startQuickSort')
+const array = [2, 4, 7, 3, 12, 11, 23, 45, 23, 1, 5, 7, 7, 3, 34, 456, 678, 23434, 676, 32, 12, 1, 3, 4, 5];
 
-			console.log(quickSort([...array]));
+console.time('startQuickSort')
 
-		console.timeEnd('startQuickSort')
+console.log(quickSort([...array]));
+
+console.timeEnd('startQuickSort')
