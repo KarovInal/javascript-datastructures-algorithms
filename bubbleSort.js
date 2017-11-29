62038c3370ec830b038a1e78d3999b3933eb7725
Jamik Karimov <jkarimov1593@gmail.com>
2017-11-29 12:02:19 +0300

    add comments

--- bubbleSort.js
+++ bubbleSort.js
@@ -8,7 +8,9 @@ function bubbleSort(arr, cmp) {
   var temp;
   for (var i = 0, l = arr.length; i < l; i++) {
     for (var j = i; j > 0; j--) {
+      //Сравниваем элементы массива
       if (cmp(arr[j], arr[j - 1]) < 0) {
+        //Меняем местами элементы массива
         temp = arr[j];
         arr[j] = arr[j - 1];
         arr[j - 1] = temp;
